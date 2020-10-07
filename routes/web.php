890e791cecc277dddc87230d039c98436b69a933
Frances D Pasulohan <franpas12123@gmail.com>
2020-10-08 04:16:49 +0800

    Update routes
    Added initial routes for navigation: signin, signup, profile, challenges, mybadges, about, blog, podcast

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/signin', function () {
+    return view('auth/signup');
+});
+
+Route::get('/signup', function () {
+    return view('auth/signin');
+});
+
+Route::get('/profile', function () {
+    return view('user/profile');
+});
+
+Route::get('/challenges', function () {
+    return view('challenges');
+});
+
+Route::get('/mybadges', function () {
+    return view('user/mybadges');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/podcast', function () {
+    return view('podcast');
 });
