476a8aade615bae73f8d8a156a36f6576256f9eb
Frances D Pasulohan <franpas12123@gmail.com>
2020-10-08 04:10:14 +0800

    Update create_users_table migration
    Added username, country, occupation, about, website, and points column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('country');
+            $table->string('occupation');
+            $table->string('about');
+            $table->string('website');
+            $table->integer('points');
             $table->rememberToken();
             $table->timestamps();
         });
