e4b8670f2ef315d2a2a293762ce5e0a555a2c65d
Frances D Pasulohan <franpas12123@gmail.com>
2020-10-08 04:12:23 +0800

    Create create_user_challenges_table
    Table to store challenges for different users

---
+++ database/migrations/2020_10_07_193244_create_user_challenges_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserChallengesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_challenges', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('challenge_id')->constrained('challenges');
+            $table->string('status');
+            $table->boolean('completed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_challenges');
+    }
+}
