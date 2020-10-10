9f69daf012af83fa5d17bd228b9079f7c95ad31f
Frances D Pasulohan <franpas12123@gmail.com>
2020-10-11 06:31:24 +0800

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.layout')
 
 @section('content')
+    <div class="img-header mb-4">
+        <img src="{{ asset('img/mockupbadges_page.jpg') }}" class="img-fluid about-img" alt="">
+    </div>
     <h1>About Page</h1>
 @endsection
