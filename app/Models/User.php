803b7b6569739d7b29470b449a79d996e61dbce9
Frances D Pasulohan <franpas12123@gmail.com>
2020-10-11 06:32:29 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'country',
+        'occupation',
+        'about',
+        'website'
     ];
 
     /**
