ce7d0825d12bb956ce59f9831971913d7897ba4a
Frances D Pasulohan <franpas12123@gmail.com>
2020-10-11 06:30:53 +0800

    Create unauthorized.blade.php

---
+++ resources/views/auth/unauthorized.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="container admin-only">
+   <div class="row align-items-center">
+     <h1 class="mx-auto">This page is for Admin only.</h1>
+   </div>
+</div>
+@endsection
