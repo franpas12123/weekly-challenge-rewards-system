c05a8c0bd3d93a2a19c7354404000c8d42d777c9
Frances D Pasulohan <franpas12123@gmail.com>
2020-10-14 02:52:59 +0800

    Added final header image

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="img-header mb-4">
-        <img src="{{ asset('img/mockupbadges_page.jpg') }}" class="img-fluid about-img" alt="">
+        <img src="{{ asset('img/black_kind_header.jpg') }}" class="img-fluid about-img" alt="">
     </div>
     <h1>About Page</h1>
 @endsection
