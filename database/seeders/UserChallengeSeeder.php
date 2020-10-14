0c472c57c49f37334280e6d17e9046f28cbd7fba
Frances D Pasulohan <franpas12123@gmail.com>
2020-10-14 19:59:29 +0800

    Update UserChallengeSeeder.php

--- database/seeders/UserChallengeSeeder.php
+++ database/seeders/UserChallengeSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserChallengeSeeder extends Seeder
 {
@@ -14,8 +15,8 @@ class UserChallengeSeeder extends Seeder
     public function run()
     {
         DB::table('user_challenges')->insert([
-            'user_id' => 'Franz',
-            'challenge_id' => 'franz@mail.com',
+            'user_id' => 2,
+            'challenge_id' => 1,
             'status' => 'Ran 5 miles',
             'completed' => 1
         ]);
