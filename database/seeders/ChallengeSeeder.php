f10256dafbc8dc12eb460946ff013f89473a0bfa
Frances D Pasulohan <franpas12123@gmail.com>
2020-10-14 19:59:23 +0800

    Update ChallengeSeeder.php

--- database/seeders/ChallengeSeeder.php
+++ database/seeders/ChallengeSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ChallengeSeeder extends Seeder
 {
@@ -14,20 +15,33 @@ class ChallengeSeeder extends Seeder
     public function run()
     {
         DB::table('challenges')->insert([
-            'name ' => 'Week 1 Challenge',
+            'name' => 'Week 1 Challenge',
             'points' => 5,
         ]);
+
         DB::table('challenges')->insert([
-            'name ' => 'Week 2 Challenge',
+            'name' => 'Week 2 Challenge',
             'points' => 6,
         ]);
+
         DB::table('challenges')->insert([
-            'name ' => 'Week 3 Challenge',
+            'name' => 'Week 3 Challenge',
             'points' => 8,
         ]);
+
         DB::table('challenges')->insert([
-            'name ' => 'Week 4 Challenge',
+            'name' => 'Week 4 Challenge',
             'points' => 10,
+        ]);
+
+        DB::table('challenges')->insert([
+            'name' => 'Week 5 Challenge',
+            'points' => 12,
+        ]);
+
+        DB::table('challenges')->insert([
+            'name' => 'Week 6 Challenge',
+            'points' => 15,
         ]);
     }
 }
