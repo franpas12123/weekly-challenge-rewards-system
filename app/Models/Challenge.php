f83208333d96b4aaa8beb70ace1996141c371a3f
Frances D Pasulohan <franpas12123@gmail.com>
2020-10-14 19:59:05 +0800

    Update Challenge.php

--- app/Models/Challenge.php
+++ app/Models/Challenge.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Challenge extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'points',
+    ];
 }
