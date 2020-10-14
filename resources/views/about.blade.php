75aa95bdab537a4c36b53d20b72c2f649c714613
Frances D Pasulohan <franpas12123@gmail.com>
2020-10-14 21:29:43 +0800

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="container-fluid vh-100">
+    <div class="container vh-100">
         <div class="row align-items-center h-75">
             <img src="{{ asset('/img/black_kind_about_us.jpg') }}" class="col img-fluid" alt="">
         </div>
