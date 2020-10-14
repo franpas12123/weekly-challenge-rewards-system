b3857e16f7d34fe1dff26f1402a7682726868948
Frances D Pasulohan <franpas12123@gmail.com>
2020-10-14 19:59:40 +0800

    Create edit.blade.php

---
+++ resources/views/user/edit.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.layout')
+
+@section('content')
+   <main role="main" class="inner cover">
+      <div class="container col-md-6 mx-auto">
+         <h1 class="display-3">{{ $user->name }}</h1>
+         <p class="lead">{{ $user->about }}</p>
+         
+         <div class="card mb-3 text-dark mt-5">
+            <div class="profile-pic-container">
+               <img src="{{ asset('img/user-circle.png') }}" class="profile-pic" alt="...">
+            </div>
+            <div class="card-body">
+               <div class="d-flex justify-content-center col-lg-10 mx-auto">
+                  <table class="table table-light user-details-table">
+                     <tbody>
+                        <tr>
+                           <td scope="row">Email</td>
+                           <td class="text-center"><input type="text" value="{{ $user->email }}"></td>
+                        </tr>
+                        <tr>
+                           <td scope="row">Country</td>
+                           <td class="text-center">{{ $user->country }}</td>
+                        </tr>
+                        <tr>
+                           <td scope="row">Occupation</td>
+                           <td class="text-center">{{ $user->occupation }}</td>
+                        </tr>
+                        <tr>
+                           <td scope="row">Website</td>
+                           <td class="text-center">{{ $user->website }}</td>
+                        </tr>
+                        <tr>
+                           <td scope="row">Level</td>
+                           <td class="text-center">{{ $user->level }}</td>
+                        </tr>
+                     </tbody>
+                  </table>
+               </div>
+               <form action="" method="post">
+                  <button class="btn btn-primary" type="submit">Edit</button>
+               </form>
+            </div>
+          </div>
+      </div>
+   </main>
+@endsection
