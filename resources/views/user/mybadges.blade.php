d8110057456b402734c06c0fd807e4f478a48c04
Frances D Pasulohan <franpas12123@gmail.com>
2020-10-14 14:20:27 +0800

    Update my badge page.
    Removed "My Badges Page" h1
    Added header
    Added points table

--- resources/views/user/mybadges.blade.php
+++ resources/views/user/mybadges.blade.php
@@ -1,5 +1,32 @@
 @extends('../layouts/layout')
 
 @section('content')
-   <h1>My Badges Page</h1>
+   <div class="container mb-4">
+      <img src="{{ asset('img/black_kind_header.jpg') }}" class="img-fluid" alt="">
+   </div>
+
+   <div class="d-flex justify-content-center mt-5">
+      <table class="table table-light col-sm-10 col-md-8 col-lg-6">
+         <thead class="thead-dark">
+            <tr>
+               <th scope="col">Name</th>
+               <th scope="col">{{ Auth::user()->name }}</th>
+            </tr>
+            </thead>
+            <tbody>
+               @foreach ($challenges as $challenge)
+                  <tr>
+                     <td scope="row">{{ $challenge->name }}</td>
+                     @guest
+                        <td>{{ $challenge->points }}</td>
+                     @endguest
+                  </tr>
+               @endforeach
+               <tr>
+                  <td>Level</td>
+                  
+               </tr>
+            </tbody>
+      </table>
+   </div>
 @endsection
